de82f517fe36cb34d8a4514dbcfff4597bf92125
rmolines <rafaelmismael@gmail.com>
2017-05-07 23:03:15 -0300

    Desafio 0

--- Desafio0/ex0.c
+++ Desafio0/ex0.c
@@ -17,7 +17,6 @@ int is_ordered (int vec[], int n){
 
 void fill_random_list (int vec[], int n){
   int i;
-  //srand(time(NULL));
 
   for (i=0; i<=n; i++){
     vec[i] = rand()%100;
@@ -75,20 +74,18 @@ void r_bubble_sort(int vec[], int n){
 }
 
 void selection_sort(int vec[], int n){
-  int i, j, smallest, swap, j_pos;
+  int i, j, swap, min_pos;
 
-  for(i=0; i<n; i++){
-    smallest=vec[i];
-    for(j=n-1; j>=i; j--){
-      if(vec[j]<smallest){
-        smallest=vec[j];
-        j_pos=j;
+  for(i=0; i<n-1; i++){
+    min_pos=i;
+    for(j=i+1; j<n; j++){
+      if(vec[j]<vec[min_pos]){
+        min_pos=j;
       }
     }
-    //printf("{%d} ", smallest);
     swap = vec[i];
-    vec[i]=smallest;
-    vec[j_pos]=swap;
+    vec[i]=vec[min_pos];
+    vec[min_pos]=swap;
   }
 }
 
@@ -113,35 +110,31 @@ void r_selection_sort(int vec[], int n){
 }
 
 void insertion_sort (int vec[], int n){
-  int i, j, swap;
+  int i, j, temp;
 
-  for (i=0; i<n-1; i++){
-    for (j=i; j>=0; j--){
-      if (vec[j+1]<vec[j]){
-        swap = vec[j+1];
-        vec[j+1] = vec[j];
-        vec[j] = swap;
-      }
+  for (i=1; i<n; i++){
+    temp = vec[i];
+    for (j=i-1; j>=0 && vec[j]>temp; j--){
+      vec[j+1] = vec[j];
     }
+    vec[j+1] = temp;
   }
 }
 
-void r_insertion_sort(int vec[], int n){
-  int j, swap;
+void r_insertion_sort(int vec[], int n, int i){
+  int j, temp;
 
-  if (n==0){
+  if (i==n){
     return;
   }
 
-  for (j=n; j>=0; j--){
-    if (vec[j+1]<vec[j]){
-      swap = vec[j+1];
-      vec[j+1] = vec[j];
-      vec[j] = swap;
-    }
+  temp = vec[i];
+  for (j=i-1; j>=0 && vec[j]>temp; j--){
+    vec[j+1] = vec[j];
   }
+  vec[j+1] = temp;
 
-  return r_insertion_sort (vec, n-1);
+  return r_insertion_sort (vec, n, i+1);
 
 }
 
@@ -155,24 +148,43 @@ int test (int vec[], int n, int times, int type){
         fill_random_list(vec, n);
         bubble_sort(vec, n);
         right_times += is_ordered(vec,n);
-        break;
       }
+      break;
     case 2:
       for(i=0; i<times; i++){
         fill_random_list(vec, n);
         selection_sort(vec, n);
         right_times += is_ordered(vec,n);
-        break;
-
       }
+      break;
     case 3:
       for(i=0; i<times; i++){
         fill_random_list(vec, n);
         insertion_sort(vec, n);
         right_times += is_ordered(vec,n);
-        break;
-
       }
+      break;
+    case 4:
+      for(i=0; i<times; i++){
+        fill_random_list(vec, n);
+        r_bubble_sort(vec, n);
+        right_times += is_ordered(vec,n);
+      }
+      break;
+    case 5:
+      for(i=0; i<times; i++){
+        fill_random_list(vec, n);
+        r_selection_sort(vec, n);
+        right_times += is_ordered(vec,n);
+      }
+      break;
+    case 6:
+      for(i=0; i<times; i++){
+        fill_random_list(vec, n);
+        r_insertion_sort(vec, n, 1);
+        right_times += is_ordered(vec,n);
+      }
+      break;
   }
 
   return right_times;
@@ -181,16 +193,17 @@ int test (int vec[], int n, int times, int type){
 
 
 void main (void){
-  int n = 100;
-  int size = 1000;
-  int times = 100;
-  int right_times;
-  int vec[size];
+  int n = 1000;
+  int times = 1000;
+  int vec[n];
   srand(time(NULL));
 
   printf("Você acertou %d vezes de %d com o bubble_sort\n", test(vec, n, times, 1), times);
   printf("Você acertou %d vezes de %d com o selection_sort\n", test(vec, n, times, 2), times);
   printf("Você acertou %d vezes de %d com o insertion_sort\n", test(vec, n, times, 3), times);
+  printf("Você acertou %d vezes de %d com o r_bubble_sort\n", test(vec, n, times, 4), times);
+  printf("Você acertou %d vezes de %d com o r_selection_sort\n", test(vec, n, times, 5), times);
+  printf("Você acertou %d vezes de %d com o r_insertion_sort\n", test(vec, n, times, 6), times);
 
 
 }
